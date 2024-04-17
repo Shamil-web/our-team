3093ccf189593f55df8de58063fa13b96c83e9e2
Shoma <beidullaevshamil@gmail.com>
2024-04-17 12:08:49 +0400

    feat: Imported the store into the main file and wrapped the App component in a Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
